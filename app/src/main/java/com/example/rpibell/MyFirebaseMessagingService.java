a4bccb2afaa79510508091823d8121af225a3c0b
Evan Nemetz <37675159+enemetz@users.noreply.github.com>
2020-11-02 20:53:21 -0500

    Notifications set up

---
+++ app/src/main/java/com/example/rpibell/MyFirebaseMessagingService.java
@@ -0,0 +1,26 @@
+package com.example.rpibell;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+    }
+
+    @Override
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+    }
+
+
+}
