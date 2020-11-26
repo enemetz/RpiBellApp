ace7e23c345cb425cfec740737972b35cc616263
Evan Nemetz <37675159+enemetz@users.noreply.github.com>
2020-11-25 20:26:53 -0500

    Added password reset and started splash screen.

---
+++ app/src/main/java/com/example/rpibell/SplashScreenPage.java
@@ -0,0 +1,14 @@
+package com.example.rpibell;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreenPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_page);
+    }
+}
